f3e1ea6fe9e21b917f54c7b51ec423254d0d51a8
ebrukhis <62902250+ebrukhis@users.noreply.github.com>
2020-04-22 13:05:55 +0300

    Запросы к таблице shop

---
+++ task5/shop_query.sql
@@ -0,0 +1,12 @@
+use shop;
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+SELECT * FROM shop.storehouses_products
+order by value desc;
